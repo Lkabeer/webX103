91e7339c3a6d181d58d0bb1db03706d3d313e44a
Lkabeer <ahmed@Lkabeer.com>
2018-09-08 20:45:58 +0200

    version X0.1

---
+++ README.md
@@ -0,0 +1,1 @@
+# In the name of Allah WebX 101
---
+++ js/script.js
@@ -0,0 +1,50 @@
+var addFormX = document.getElementById('addFormX');
+var membersX = document.getElementById('membersX');
+var newMember = document.getElementById('newMemberX')
+var filterX = document.getElementById('filterX');
+
+// X-Team Events
+addFormX.addEventListener('submit', addMember);
+membersX.addEventListener('click', removeMember);
+filterX.addEventListener('keyup', filterMembers);
+
+// Add Member 
+function addMember(e) {
+    e.preventDefault();
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.textContent = newMember.value;
+    newMember.value = '';
+
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right deleteX';
+    deleteBtn.textContent = 'X';
+
+    li.appendChild(deleteBtn);
+    membersX.appendChild(li);
+}
+
+// Remove Member 
+function removeMember(e) {
+    if(e.target.classList.contains('deleteX')) {
+        if(confirm('Anta mot2kd Yasta mn 2lly bt3mloh?!')) {
+            var li = e.target.parentElement;
+            membersX.removeChild(li);
+        }
+    }
+}
+
+// Filter Members
+function filterMembers(e) {
+    var textX = e.target.value.toLowerCase();
+    var membersListX = membersX.getElementsByTagName('li');
+
+    Array.from(membersListX).forEach(function(member) {
+        var memberName = member.firstChild.textContent;
+        if(memberName.toLowerCase().indexOf(textX) != -1) {
+            member.style.display = 'block';
+        } else {
+            member.style.display = 'none';
+        }
+    });
+}
